222185cd9ce8b8ea68d91983ff2d3b08b54b0ab3
Тимур Низамов <tranizamov@sberbank.ru>
2025-03-26 23:21:43 +0300

    ER diagram

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -17,6 +17,7 @@ import ru.yandex.practicum.filmorate.util.DurationSerializer;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -48,7 +49,6 @@ public class Film {
     @JsonIgnore
     Set<Long> likes = new HashSet<>();
 
-    List<String> genre;
-
-    String rating;
+    List<Genre> genres = new ArrayList<>();
+    Rating mpa;
 }
